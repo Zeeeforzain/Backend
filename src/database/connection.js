186022ae09f242bbc87e715c7ba7dbef6b970d91
Zain ul Abideen <zeeforzain@gmail.com>
2024-10-15 15:25:44 +0500

    Add files via upload

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,12 @@
+// src/database/connection.js
 const mongoose = require("mongoose");
 require("dotenv").config(); // Load environment variables from .env
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected successfully");
+        await mongoose.connect(process.env.MONGO_URI, {
+         })
+         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
         process.exit(1); // Exit the process with failure
@@ -12,4 +14,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
